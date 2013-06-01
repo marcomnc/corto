6f6a09079f908a2b9123a705c796037bd0327092
doctor <doctor@TARDIS>
2013-06-01 02:49:19 +0200

    Iniziata gestione globale da gastione zone

--- app/code/local/Autel/Corto/sql/autelcorto_setup/mysql4-upgrade-0.1.2-0.1.3.php
+++ app/code/local/Autel/Corto/sql/autelcorto_setup/mysql4-upgrade-0.1.2-0.1.3.php
@@ -5,39 +5,23 @@ $installer = $this;
 
 $installer->startSetup();
 
-
-$table_group = $installer->getConnection()
-    ->newTable($installer->getTable('autelcorto/zonegroup'))
-    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'nullable'  => false,
-        'auto_increment' => true,
-        'primary'   => true,
-        ), 'Entity ID')
-    ->addColumn('group_name', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-        'nullable'  => false,        
-        ), 'Nome del gruppo')
-    ->addColumn('sort_order', Varien_Db_Ddl_Table::TYPE_INTEGER, 6, array(
-        'nullable'  => true,        
-        ), 'Ordinamento');
-$installer->getConnection()->createTable($table_group);
-
-
-$this->getConnection()->addColumn($this->getTable('autelcorto/zone'), 'group_id', Varien_Db_Ddl_Table::TYPE_SMALLINT);
-$this->getConnection()->addForeignKey($installer->getFkName('autelcorto/zone', 'group_id', 'autelcorto/zonegroup', 'entity_id'),
-        $this->getTable('autelcorto/zone'), 'group_id', $this->getTable('autelcorto/zonegroup'), 'entity_id',
-        Varien_Db_Ddl_Table::ACTION_RESTRICT, Varien_Db_Ddl_Table::ACTION_RESTRICT);
-
-$groupBase = Mage::getModel('autelcorto/zonegroup');
-$groupBase->setGroupName('Base');
-$groupBase->setSortOrder(0);
-$groupBase->save();
+$this->getConnection()->addColumn($this->getTable('autelcorto/zone'), 'store_id', 
+        array("COLUMN_TYPE" =>  Varien_Db_Ddl_Table::TYPE_TEXT, 
+              "LENGTH" => 50, 
+              "COMMENT" => 'Lista store associati alla zona (per identificare lingue disponibili)'));
 
 foreach (Mage::getModel('autelcorto/zone')->getCollection() as $z) {
     $zone = Mage::getModel('autelcorto/zone')->Load($z->getId());
-    $zone->setGroupId($groupBase->getId());
+    $storeId = '';
+    foreach (Mage::getModel('core/store')->getCollection() as $store) {
+        if ($store->getWebsiteId() == $zone->getWebsiteId()) {
+            $storeId .= ($storeId != '') ? ',' : '';
+            $storeId .= $store->getId();
+        }
+    }
+    $zone->setStoreId($storeId);
     $zone->save();
 }
-
 
 $installer->endSetup();
 
