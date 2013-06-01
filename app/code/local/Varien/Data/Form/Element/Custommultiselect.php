125b9641c914c0a776c3a681bc9710f4c94d405f
doctor <doctor@TARDIS>
2013-06-01 02:15:09 +0200

    Creato il controllo custom multiselect

--- app/code/local/Varien/Data/Form/Element/Custommultiselect.php
+++ app/code/local/Varien/Data/Form/Element/Custommultiselect.php
@@ -25,8 +25,10 @@ class Varien_Data_Form_Element_Custommultiselect extends Varien_Data_Form_Elemen
 {
     public function getElementHtml()
     {
-        $this->addClass('select');        
-        $html = '<ul id="'.$this->getHtmlId().'" '.$this->serialize($this->getHtmlAttributes()).'>'."\n";
+        $this->addClass('select custom-multiselect');        
+        $html  = '<div id="'.$this->getHtmlId().'" class="custom-multiselect-container">';
+        $html .= $this->getScriptHtml();
+        $html .= '<ul '.$this->serialize($this->getHtmlAttributes()).'>'."\n";
         
         $value = $this->getValue();
         if (!is_array($value)) {
@@ -53,17 +55,26 @@ class Varien_Data_Form_Element_Custommultiselect extends Varien_Data_Form_Elemen
                 }
             }
         }        
-        $html .= '</ul>';
-
+        $html .= '</ul><div class="action">';
+        $html .= '<button onclick="' . $this->getJsObjectName() . '.SelectAll();" class="scalable save" type="button">';
+        $html .= '<span>&nbsp;</span></button>';
+        $html .= '<button onclick="' . $this->getJsObjectName() . '.DeselectAll();" class="scalable delete fail" type="button">';
+        $html .= '<span>&nbsp;</span></button>';
+        $html .= "</div></div>";
+        
         return $html;
     }
     
     protected function _optionToHtml($item, $selected) {
         
         $htmlOpt  = '<li' . ((isset($item['style'])) ? 'style="'.$item['style'].'"' : '') . '>';
-        $htmlOpt .= '<label for="'. $this->getJsObjectName() . '_' . $item['value'] .'">';
-        $htmlOpt .= '<input type="checkbox" name="' . $this->getName() . '[]"';
-        $htmlOpt .= '  id="'. $this->getJsObjectName() . '_' . $item['value'] .'" value="' . $item['value'] .'" class="checkbox"';
+        $htmlOpt .= '<label onclick="'.$this->getJsObjectName().'.CheckRow(\'' . $item['value'] .'\');" id="label_' .  $this->getJsObjectName() . '_' . $item['value'] .'" for="'. $this->getJsObjectName() . '_' . $item['value'] .'"  rel="' . $item['value'] .'"';
+        if (in_array((string)$item['value'], $selected)) {
+            $htmlOpt .= ' class="selected"';
+        }
+        $htmlOpt .= '>';
+        $htmlOpt .= '<input onclick="'.$this->getJsObjectName().'.CheckRow(\'' . $item['value'] .'\');" type="checkbox" name="' . $this->getName() . '[]"  rel="' . $item['value'] .'"';
+        $htmlOpt .= '  id="check_'. $this->getJsObjectName() . '_' . $item['value'] .'" value="' . $item['value'] .'" class="checkbox"';
         if (in_array((string)$item['value'], $selected)) {
             $htmlOpt .= ' checked';
         }
@@ -83,6 +94,40 @@ class Varien_Data_Form_Element_Custommultiselect extends Varien_Data_Form_Elemen
     {
         return array('title', 'class', 'style', 'onclick', 'onchange', 'disabled', 'size', 'tabindex');
     }
+    
+    public function getScriptHtml() {
+        $result  = '<script>' . "\n";
+        $result .= '    var ' . $this->getJsObjectName() . ' = {' . "\n";
+        $result .= '        SelectAll: function() {' . "\n";
+        $result .= '            $$("#' . $this->getHtmlId() .' ul li label").each( function(el) {' . "\n";
+        $result .= '                   var rel=$(el).readAttribute("rel");' . "\n";
+        $result .= '                   $("check_' . $this->getJsObjectName() .'_"+rel).checked = true;' . "\n";
+        $result .= '                   $("label_' . $this->getJsObjectName() .'_"+rel).addClassName("selected");' . "\n";
+        $result .= '            }); ' . "\n";
+        $result .= '        },' . "\n";
+        $result .= '        DeselectAll: function() {' . "\n";
+        $result .= '            $$("#' . $this->getHtmlId() .' ul li label").each( function(el) {' . "\n";
+        $result .= '                   var rel=$(el).readAttribute("rel");' . "\n";
+        $result .= '                   $("check_' . $this->getJsObjectName() .'_"+rel).checked = false;' . "\n";
+        $result .= '                   $("label_' . $this->getJsObjectName() .'_"+rel).removeClassName("selected");' . "\n";
+        $result .= '            }); ' . "\n";
+        $result .= '        },' . "\n";
+        $result .= '        CheckRow: function(rel) {' . "\n";        
+        $result .= '            if (!$("check_' . $this->getJsObjectName() .'_"+rel).checked) {' . "\n";
+        $result .= '                $("check_' . $this->getJsObjectName() .'_"+rel).checked = true;' . "\n";
+        $result .= '                $("label_' . $this->getJsObjectName() .'_"+rel).addClassName("selected");' . "\n";
+        $result .= '            } else {' . "\n";
+        $result .= '                $("label_' . $this->getJsObjectName() .'_"+rel).removeClassName("selected");' . "\n";
+        $result .= '                $("check_' . $this->getJsObjectName() .'_"+rel).checked = false;' . "\n";
+        $result .= '            }' . "\n";
+        $result .= '            return false;' . "\n";
+        $result .= '        }' . "\n";
+        $result .= '    }' . "\n";
+        $result .= '</script>' . "\n";
+        return $result;
+    }
 }
 
 ?>
+
+
